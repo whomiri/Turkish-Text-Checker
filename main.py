b01c76a8f203f0789cf842b95d302921a94ad97f
whomiri <75207175+whomiri@users.noreply.github.com>
2022-01-14 01:20:17 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+
+os.system(request)
+
+request{
+
+!pip3 install git
+!pip3 install request
+!pip3 install ireq
+!python3 -m "https://github.com/whomiri/Turkish-Text-Checker/Yazim-Denetim.py"
+!python3 -m "https://github.com/whomiri/Turkish-Text-Checker/Text-Checker.py"
+!python3 -m -install
+
+}
